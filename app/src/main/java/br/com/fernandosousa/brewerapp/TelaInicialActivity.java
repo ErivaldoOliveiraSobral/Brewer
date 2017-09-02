1ab23b2f8fd9eba8bee42111e5e49d2b718fb4f7
Fernando Sousa <fernandosousa.ti@gmail.com>
2017-09-02 19:58:04 -0300

    Atualização

--- app/src/main/java/br/com/fernandosousa/brewerapp/TelaInicialActivity.java
+++ app/src/main/java/br/com/fernandosousa/brewerapp/TelaInicialActivity.java
@@ -3,6 +3,8 @@ package br.com.fernandosousa.brewerapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,4 +25,34 @@ public class TelaInicialActivity extends DebugActivity {
         TextView texto = (TextView) findViewById(R.id.textoInicial);
         texto.setText(nome);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_atualizar) {
+            Toast.makeText(TelaInicialActivity.this,
+                    "Atualizar",
+                    Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.action_buscar) {
+            Toast.makeText(TelaInicialActivity.this,
+                    "Buscar",
+                    Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.action_adicionar) {
+            Toast.makeText(TelaInicialActivity.this,
+                    "Buscar",
+                    Toast.LENGTH_SHORT).show();
+        } else if(id == R.id.action_config) {
+            Toast.makeText(TelaInicialActivity.this,
+                    "Config",
+                    Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
